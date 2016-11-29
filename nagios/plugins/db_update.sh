0fc2f16da4537d98e15c9311916d132947c672ac
nierdz <kevin.met@mnt-tech.fr>
2016-11-29 16:49:38 +0100

    Bugfix sur db_update.sh

--- nagios/plugins/db_update.sh
+++ nagios/plugins/db_update.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 integrit -C /etc/integrit/nagios.conf -u
 mv /var/lib/integrit/current.cdb  /var/lib/integrit/known.cdb
-rm /tmp/integrit-*
+find /tmp/ -name 'integrit-*' && rm -rf /tmp/integrit-*
