0c1cd8f883c6887022b2a621b97d0e3721e29fa6
nierdz <kevin.met@mnt-tech.fr>
2017-01-24 22:34:16 +0100

    BugFix: Forget 206 and 499 status codes

--- cacti/logs/parse_www_logs.pl
+++ cacti/logs/parse_www_logs.pl
@@ -10,8 +10,8 @@ my $five_minutes = strftime("%d/%b/%Y:%H:%M:%S",localtime(time-300));
 
 # Empty hash containing the status codes
 my %hash = (
-	'200' => 0,
-	'206' => 0,
+	'200' =>0,
+	'206' =>0,
 	'301' =>0,
 	'302' =>0,
 	'304' =>0,
@@ -33,43 +33,39 @@ while( defined( my $log_line = $bw->readline ) ) {
 	if ($status == 200) {
 	$hash{200}++;
 	}
-
+	if ($status == 206) {
+	$hash{206}++;
+	}
 	if ($status == 301) {
 	$hash{301}++;
 	}
-	
 	if ($status == 302) {
 	$hash{302}++;
 	}
-	
 	if ($status == 304) {
 	$hash{304}++;
 	}
-	
 	if ($status == 310) {
 	$hash{310}++;
 	}
-	
 	if ($status == 400) {
 	$hash{400}++;
 	}
-	
 	if ($status == 401) {
 	$hash{401}++;
 	}
-	
 	if ($status == 403) {
 	$hash{403}++;
 	}
-	
 	if ($status == 404) {
 	$hash{404}++;
 	}
-	
+	if ($status == 499) {
+	$hash{499}++;
+	}
 	if ($status == 500) {
 	$hash{500}++;
 	}
-	
 	if ($status == 503) {
 	$hash{503}++;
 	}
